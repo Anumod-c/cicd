f1d296aba0f65ebc2cbf1dfdc7077aa8e370231d
Anumod-c <anumodc2001@gmail.com>
2024-07-14 16:34:11 +0530

    message altered

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
  const app = express();
  const PORT =  3000;
  app.get('/',(req,res)=>{
-    res.send("Hello World from CICD and it successfully worked (3)")
+    res.send("CICD worked for the first time")
  })
 
  app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`));
