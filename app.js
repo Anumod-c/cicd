ee81644aabc3d4bd23438c48d899ef267d5a0c28
Anumod-c <anumodc2001@gmail.com>
2024-07-15 11:09:39 +0530

    final commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
  const app = express();
  const PORT =  3000;
  app.get('/',(req,res)=>{
-    res.send("CICD worked for the first time")
+    res.send("CICD worked for the 1st time")
  })
 
  app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`));
